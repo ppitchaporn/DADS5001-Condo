4b3e880af98e07f0420508fb8c4245487d354e1e
Pornchanok.T <pornchanok.tan14@gmail.com>
2025-06-12 01:07:51 +0700

    Update page1.py

--- docker/python/pages/page1.py
+++ docker/python/pages/page1.py
@@ -156,13 +156,13 @@ def main_page():
 home_page = st.Page(main_page,title='Home page', icon=":material/home:")
 descipt = st.Page("page2.py", title='Descriptive Analysis', icon=":material/database:")
 relation_predict = st.Page("page3.py", title='Relationship Analysis & Price Prediction', icon=":material/bar_chart:")
-geospatial_class = st.Page("page4.py", title='Geospatial Analysis & Classification Result', icon=":material/bar_chart:")
+geo_page = st.Page("page4.py", title='Condo Map Insights & Classification', icon=":material/bar_chart:")
 Ai_search = st.Page("page5.py", title=' Advanced Search & Comparison', icon=":material/note:")
 
 ## create menu tree
 pg = st.navigation(
     {
-        "Menu": [home_page, descipt, relation_predict, geospatial_class, Ai_search],
+        "Menu": [home_page, descipt, relation_predict, geo_page, Ai_search],
 
     }
 )
